fe89fa19d3f26a9086c79f2e689b62d39e5b56b4
Karman <ksselhi@gmail.com>
2018-08-26 17:05:06 +0530

    Fixed the cubing error in the algorithm

--- Assignment 1/problem4.py
+++ Assignment 1/problem4.py
@@ -3,60 +3,27 @@
 import os
 
 for t in range(0,13):
-  x = ((t^3)+(9*t)+5)%13
+  x = ((t**3)+(9*t)+5)%13
   if x == 1:
     print '({},1)'.format(t)
-    print '({},-1)'.format(t)
     print '({},12)'.format(t)
-    print '({},-12)'.format(t)
   elif x == 4:
     print '({},2)'.format(t)
-    print '({},-2)'.format(t)
     print '({},11)'.format(t)
-    print '({},-11)'.format(t)
   elif x == 9:
     print '({},3)'.format(t)
-    print '({},-3)'.format(t)
     print '({},10)'.format(t)
-    print '({},-10)'.format(t)
   elif x == 3:
     print '({},4)'.format(t)
-    print '({},-4)'.format(t)
     print '({},9)'.format(t)
-    print '({},-9)'.format(t)
   elif x == 12:
     print '({},5)'.format(t)
-    print '({},-5)'.format(t)
     print '({},8)'.format(t)
-    print '({},-8)'.format(t)
   elif x == 10:
     print '({},6)'.format(t)
-    print '({},-6)'.format(t)
     print '({},7)'.format(t)
-    print '({},-7)'.format(t)
-  elif x == 3:
-    print '({},4)'.format(t)
-    print '({},-4)'.format(t)
   elif x == 0:
     print '({},0)'.format(t)
   else: 
     print '{} does not have a solution'.format(t)
 
-#0 does not have a solution
-#(1,4)
-#(1,9)
-#2 does not have a solution
-#3 does not have a solution
-#(4,3)
-#(4,10)
-#(5,2)
-#(5,11)
-#(6,5)
-#(6,8)
-#7 does not have a solution
-#(8,6)
-#(8,7)
-#9 does not have a solution
-#(10,0)
-#11 does not have a solution
-#12 does not have a solution
